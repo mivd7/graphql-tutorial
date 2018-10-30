dee1a6861291ee2f13f6b06ef740a4cc1626b3b9
MIVD <mi.vandeurzen@gmail.com>
2018-10-30 17:14:49 +0100

    links + login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import LinkList from './components/LinkList'
+import CreateLink from './components/CreateLink'
+import Header from './components/Header'
+import Login from './components/Login'
 
 class App extends Component {
   render() {
-    return <LinkList />
-  }
-}
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </div>
+  )
+}}
 
 export default App
